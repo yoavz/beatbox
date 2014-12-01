28d14c5aec70a6bb4ccbba298584c13a3e3c0ec7
Yoav Zimmerman <yoavzimmerman@gmail.com>
2014-12-01 19:18:44 +0100

    added in all the instruments, some background, instr selection

--- client/templates/track/track.js
+++ client/templates/track/track.js
@@ -2,27 +2,34 @@ MAPPINGS_808 = {
   "snare": "/samples/808/snare.ogg",
   "kick": "/samples/808/kick.ogg",
   "hihat": "/samples/808/chh.ogg",
+  "clave": "/samples/808/clave.ogg",
+  "cowbell": "/samples/808/cow.ogg",
+  "rim": "/samples/808/rim.ogg",
+  "cymbal": "/samples/808/cymbal.ogg",
+  "clap": "/samples/808/clap.ogg",
 }
 
-Template.track.created = function () {
-
-  // Load the sounds through soundmanager
-  this.sound = soundManager.createSound({
-      url: MAPPINGS_808[this.data.instrument]
-  });
-
-}
 
 Template.track.rendered = function () {
 
   // this function gets run time increment
   this.autorun(function () {
-
     var self = this.templateInstance();
+
+    // if the sound is not defined OR the instrument has changed, update it
+    if (!this.sound || this.sound.instrument !== self.data.instrument) {
+
+      this.sound = soundManager.createSound({
+          url: MAPPINGS_808[self.data.instrument]
+      });
+
+      this.sound.instrument = self.data.instrument;
+    }
+
     var beat = Session.get("absoluteTime") % 16;
 
     if (Beats.find({trackId: self.data._id, pos: beat, active: true}).count() > 0)
-      self.sound.play();
+      this.sound.play();
 
   });
 
@@ -34,14 +41,30 @@ Template.track.helpers({
     return Beats.find({trackId: this._id});
   }, 
 
+  instruments: function () {
+    return _.keys(MAPPINGS_808);
+  },
+
+  isSelected: function (instr) {
+    console.log(instr);
+    return instr === this.instrument ? "selected" : "";
+  }
+
 });
 
 Template.track.events({
 
   "click .reset-button": function () {
     Meteor.call("resetTrack", this._id);
-  }
+  },
 
+  "change .select-instrument": function (e) {
+    fields = {
+      "instrument": $(e.target).find(":selected").text()
+    }
+
+    Tracks.update(this._id, {$set: fields}, false);
+  }
 });
 
 Template.beat.helpers({
