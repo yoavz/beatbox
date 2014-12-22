b2fb1467f5f49c77bd8c984744bf881eb60b69ad
Yoav Zimmerman <yoavzimmerman@gmail.com>
2014-12-22 08:16:16 +0000

    subscribe only to necessary room

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,11 @@
 Meteor.publish('Rooms', function () {
   return Rooms.find({ hidden: false });
 });
+
+Meteor.publish('Room', function (roomId) {
+  return Rooms.find(roomId);
+});
+
 Meteor.publish('Tracks', function (roomId) {
   return Tracks.find({ roomId: roomId });
 });
